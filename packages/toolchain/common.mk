1318ca5287756a9934c056588493e62f8188de43
Thierry Reding <thierry@gilfi.de>
2007-06-12 09:35:46 +0200

    Miscellaneous fixes and improvements.

--- packages/toolchain/common.mk
+++ packages/toolchain/common.mk
@@ -1,4 +1,5 @@
-env = PATH=$(PLATFORM)/usr/bin:/usr/bin:/bin
+PATH = $(PLATFORM)/usr/bin:/usr/bin:/bin
+env = PATH=$(PATH)
 CORE_PREFIX = $(PLATFORM)/bootstrap
 BUILD = $(shell echo $$MACHTYPE)
 
--- platform/toolchain/common.mk
+++ platform/toolchain/common.mk
@@ -4,5 +4,24 @@ ifdef TOOLCHAIN
   TOOLCHAIN_ROOT = $(srctree)/platform/toolchain/$(TOOLCHAIN)
   CROSS_COMPILE = $(TOOLCHAIN_ROOT)/usr/bin/$(HOST)-
   export TOOLCHAIN_ROOT CROSS_COMPILE
+
+  # FIXME: does this really belong here? it doesn't have anything to do with
+  # the toolchain
+  SYSROOT ?= $(PLATFORM)/rootfs
+  export SYSROOT
+
+  ABIFLAGS  =
+  OPTFLAGS  =
+  CPPFLAGS += \
+	--sysroot=$(SYSROOT)
+
+  CFLAGS = \
+	$(CPPFLAGS) \
+	$(ABIFLAGS) \
+	$(OPTFLAGS)
+
+  LDFLAGS =
+
+  export ABIFLAGS OPTFLAGS CPPFLAGS CFLAGS LDFLAGS
 endif
 
