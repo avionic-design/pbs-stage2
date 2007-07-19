5219f1aadc0dddffe6b3871447a96aaabd423a03
Thierry Reding <thierry@gilfi.de>
2007-07-19 13:54:11 +0200

    Cleanup and enhance the autotools.mk common script.

--- packages/autotools.mk
+++ packages/autotools.mk
@@ -1,25 +1,31 @@
 include packages/common.mk
 
-conf-args = \
+env += PATH=$(TOOLCHAIN_ROOT)/usr/bin:$(PATH)
+
+conf-args += \
 	--host=$(HOST) \
 	--prefix=/usr \
 	--infodir=\$${prefix}/share/info \
 	--mandir=\$${prefix}/share/man \
-	--sysconfdir=/etc \
-	$(call set-args, CC CFLAGS LD LDFLAGS)
+	--sysconfdir=/etc
+
+conf-vars +=
 
 autotools-configure:
-	cd $(pkgtree) && $(env) ./configure $(conf-args)
+	cd $(pkgtree) && $(env) ./configure $(conf-args) $(conf-vars)
 
-build-args =
+build-args +=
+build-vars +=
+
 autotools-build:
-	cd $(pkgtree) && $(env) $(MAKE) $(build-args)
+	cd $(pkgtree) && $(env) $(MAKE) $(build-args) $(build-vars)
 
-install-args = \
+install-args +=
+install-vars += \
 	DESTDIR=$(ROOTFS)
 
 autotools-install:
-	cd $(pkgtree) && $(env) $(MAKE) $(install-args) install
+	cd $(pkgtree) && $(env) $(MAKE) $(install-args) $(install-vars) install
 
 package-configure: package-pre-configure autotools-configure package-post-configure
 package-build: package-pre-build autotools-build package-post-build
