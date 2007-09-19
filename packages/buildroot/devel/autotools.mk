67634af6745bc49b843a4952094cdefd56cf7592
Thierry Reding <thierry@gilfi.de>
2007-09-19 10:05:05 +0200

    Don't modify the path to find "cross-compile" tools but pass variables.
    Setting the path will entail that the build system will try to use the tools
    we just install. This will for instance make coreutils try to use the just-
    built chmod in its install target. Since the tools will only run in a chroot,
    we cannot do this.

--- packages/buildroot/devel/autotools.mk
+++ packages/buildroot/devel/autotools.mk
@@ -3,3 +3,9 @@ prefix = /tools
 include packages/buildroot/devel/common.mk
 include packages/buildroot/autotools.mk
 
+conf-args += \
+	--host=$(shell $(CC) -dumpmachine)
+
+conf-vars += \
+	$(call set-args, $(CROSS_TOOLS))
+
--- packages/buildroot/devel/common.mk
+++ packages/buildroot/devel/common.mk
@@ -1,4 +1,32 @@
 include packages/buildroot/common.mk
 
-env = env -i PATH=$(ROOTFS)/tools/bin:/usr/bin:/bin
+CROSS_COMPILE = $(ROOTFS)/tools/bin/
+AR      = $(CROSS_COMPILE)ar
+AS      = $(CROSS_COMPILE)as
+CC      = $(CROSS_COMPILE)gcc
+CPP     = $(CROSS_COMPILE)cpp
+CXX     = $(CROSS_COMPILE)g++
+LD      = $(CROSS_COMPILE)ld
+OBJCOPY = $(CROSS_COMPILE)objcopy
+OBJDUMP = $(CROSS_COMPILE)objdump
+RANLIB  = $(CROSS_COMPILE)ranlib
+READELF = $(CROSS_COMPILE)readelf
+STRIP   = $(CROSS_COMPILE)strip
+
+CFLAGS   = --sysroot $(ROOTFS)
+CXXFLAGS = --sysroot $(ROOTFS)
+LDFLAGS  = --sysroot $(ROOTFS)
+
+CROSS_TOOLS = \
+	AR \
+	AS \
+	CC \
+	CPP \
+	CXX \
+	LD \
+	OBJCOPY \
+	OBJDUMP \
+	RANLIB \
+	READELF \
+	STRIP
 
