622546e36d45db4e99e74f97b2996f49e361d25a
Thierry Reding <thierry@gilfi.de>
2009-05-22 13:11:44 +0200

    Fix support for platform customizations.

--- packages/common.mk
+++ packages/common.mk
@@ -37,16 +37,3 @@ set-args = $(foreach arg, $(1), $(arg)='$($(arg))')
 priv = sudo
 env  = env -i PATH=$(TOOLCHAIN_ROOT)$(tprefix)/bin:$(PATH)
 export priv env
-
-quiet_cmd_install = INSTALL   $(subst $(ROOTFS),,$3)
-      cmd_install = $(priv) install $2 $3
-
-quiet_cmd_install_bin = INSTALL   $(subst $(ROOTFS),,$3)
-      cmd_install_bin = $(priv) install --mode 755 $2 $3
-
-quiet_cmd_install_dir = INSTALL   $(subst $(ROOTFS),,$2)
-      cmd_install_dir = $(priv) install -d --mode 755 $2
-
-quiet_cmd_link = LN        $(subst $(ROOTFS),,$3) -> $2
-      cmd_link = $(priv) ln -sf $2 $3
-
