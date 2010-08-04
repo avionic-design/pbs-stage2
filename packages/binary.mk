e5d6d8bcc86cf085b17687952455aefd9044eaad
Thierry Reding <thierry.reding@avionic-design.de>
2010-08-04 14:21:53 +0200

    build system: Embed target in package filenames.
    Instead of sorting the packages into subdirectories identifying the
    architecture/processor and C library which the packages are built
    against, package filenames now contain the complete target configuration
    string, which is also used in the toolchain prefix.
    
    In fact, categorizing by architecture/processor and C library is
    actually not enough, because it doesn't account for the ABI, the
    operating system and potentially other factors.

--- packages/binary.mk
+++ packages/binary.mk
@@ -1,22 +1,15 @@
-ARCH := $(subst $(quote),,$(CONFIG_ARCH))
-CPU := $(subst $(quote),,$(CONFIG_CPU))
-LIBC := $(subst $(quote),,$(CONFIG_LIBC))
-ABI := $(subst $(quote),,$(CONFIG_ABI))
-
 $(pkgtree)/.binary: $(pkgtree)/.do-install $(pkgtree)/.cleanup
 	cd $(pkgsrctree) && \
 		$(priv) $(srctree)/scripts/pbs-install \
-			-a $(ARCH) -l $(LIBC) -s $(DESTDIR) \
-			-b $(objtree)/binary
+			-s $(DESTDIR) -b $(objtree)/binary
 	cd $(pkgsrctree) && \
 		$(priv) $(srctree)/scripts/pbs-binary \
-			-a $(ARCH) -l $(LIBC) -v $(VERSION) \
-			-b $(objtree)/binary
+			-t $(TARGET) -v $(VERSION) -b $(objtree)/binary
 	$(call cmd,stamp)
 
 $(pkgtree)/.install: $(pkgtree)/.binary
 	cd $(pkgsrctree) && \
 		$(priv) $(srctree)/scripts/pbs-extract \
-			-a $(ARCH) -l $(LIBC) -v $(VERSION) -r $(SYSROOT) \
+			-t $(TARGET) -v $(VERSION) -r $(SYSROOT) \
 			-b $(objtree)/binary
 	$(call cmd,stamp)
