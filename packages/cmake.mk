3f862b088e8337efe0ea55c42332515be5e36dcd
Thierry Reding <thierry.reding@avionic-design.de>
2010-11-15 14:03:06 +0100

    Add common build script for CMake-based packages.
    This build script allows cross-building packages based on the CMake
    build system. Before starting a build, a special cross-compilation CMake
    snippet is generated, substituting the correct values for the target
    toolchain as well as the target sysroot.

---
+++ packages/cmake.mk
@@ -0,0 +1,41 @@
+include packages/common.mk
+
+env += \
+	PKG_CONFIG_LIBDIR=$(SYSROOT)$(prefix)/lib/pkgconfig \
+	PKG_CONFIG_SYSROOT_DIR=$(SYSROOT)
+
+quiet_cmd_gen_cmake = GEN     $@
+      cmd_gen_cmake = sed 's|@TARGET@|$(TARGET)|;s|@SYSROOT@|$(SYSROOT)|' $< > $@
+
+$(pkgtree)/cross-compile.cmake: $(srctree)/support/cross-compile.cmake.in $(pkgtree)/.extract
+	$(call cmd,gen_cmake)
+
+$(pkgtree)/.setup: $(pkgtree)/cross-compile.cmake
+	$(call cmd,stamp)
+
+cmake-args = \
+	-DCMAKE_TOOLCHAIN_FILE=$(pkgtree)/cross-compile.cmake \
+	-DPREFIX=$(prefix) \
+	-DSYSCONFDIR=/etc
+
+$(pkgtree)/.configure: $(pkgtree)/.patch
+	mkdir -p $(pkgbuildtree)/obj-$(TARGET) && \
+		cd $(pkgbuildtree)/obj-$(TARGET) && \
+			$(env) cmake $(cmake-args) ..
+	$(call cmd,stamp)
+
+$(pkgtree)/.build: $(pkgtree)/.configure
+	cd $(pkgbuildtree)/obj-$(TARGET) && \
+		$(env) $(MAKE)
+	$(call cmd,stamp)
+
+install-args = \
+	DESTDIR=$(DESTDIR)
+
+$(pkgtree)/.do-install: $(pkgtree)/.build
+	cd $(pkgbuildtree)/obj-$(TARGET) && \
+		$(env) $(priv) $(MAKE) $(install-args) install
+	$(call cmd,stamp)
+
+include packages/cleanup.mk
+include packages/binary.mk
