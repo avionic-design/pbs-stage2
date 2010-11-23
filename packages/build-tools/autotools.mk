0804bccda2d8c98585b63c9efbb95a7dc647a335
Thierry Reding <thierry.reding@avionic-design.de>
2010-11-23 11:00:07 +0100

    build-tools/autotools.mk: Define BUILD_GNU_TYPE.
    This fixes a harmless issue where the build directory would not be
    defined properly, because of the missing definition of the
    BUILD_GNU_TYPE variable.

--- packages/build-tools/autotools.mk
+++ packages/build-tools/autotools.mk
@@ -1,5 +1,7 @@
 include packages/build-tools/common.mk
 
+BUILD_GNU_TYPE = $(shell $(srctree)/support/config.guess)
+
 env += \
 	PKG_CONFIG_PATH=$(prefix)/lib/pkgconfig \
 	LD_LIBRARY_PATH=$(prefix)/lib
@@ -11,18 +13,17 @@ conf-args = \
 	--prefix=$(prefix)
 
 $(pkgtree)/.configure: $(pkgtree)/.patch
-	mkdir -p $(pkgbuildtree)/obj-$(BUILD) && \
-		cd $(pkgbuildtree)/obj-$(BUILD) && \
+	mkdir -p $(pkgbuildtree)/obj-$(BUILD_GNU_TYPE) && \
+		cd $(pkgbuildtree)/obj-$(BUILD_GNU_TYPE) && \
 			$(env) ../configure $(conf-args) $(conf-vars)
 	$(call cmd,stamp)
 
 $(pkgtree)/.build: $(pkgtree)/.configure
-	cd $(pkgbuildtree)/obj-$(BUILD) && \
-		$(env) $(MAKE)
+	cd $(pkgbuildtree)/obj-$(BUILD_GNU_TYPE) && \
+		$(env) $(MAKE) $(build-args)
 	$(call cmd,stamp)
 
 $(pkgtree)/.do-install: $(pkgtree)/.build
-	cd $(pkgbuildtree)/obj-$(BUILD) && \
-		$(env) $(MAKE) install
+	cd $(pkgbuildtree)/obj-$(BUILD_GNU_TYPE) && \
+		$(env) $(MAKE) $(install-args) install
 	$(call cmd,stamp)
-
