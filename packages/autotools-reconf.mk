41bf25eb6acee6db71fb22c9e79f9b67de2577d6
Thierry Reding <thierry.reding@avionic-design.de>
2011-05-27 09:01:23 +0200

    packages: Add common autoreconf build script.
    This script can be used for packages that need to be reconfigured
    because of changes to the autotools-based build system.

---
+++ packages/autotools-reconf.mk
@@ -0,0 +1,8 @@
+include packages/autotools.mk
+
+$(pkgtree)/.autoreconfigure:
+	cd $(pkgbuildtree) && \
+		$(env) autoreconf --force --install
+	$(call cmd,stamp)
+
+$(pkgtree)/.configure: $(pkgtree)/.autoreconfigure
