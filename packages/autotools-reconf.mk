d7d2dcea06e26ef39f226557aaae413ff0f55ec4
Thierry Reding <thierry.reding@avionic-design.de>
2011-10-25 11:28:25 +0200

    toolchain: Promote libtool package to toolchain.
    libtool really belongs to the toolchain. Eventually all autotools-based
    packages should be reconfigured to make sure they use the toolchain's
    libtool instead of what they ship with (some libtool implementations are
    inherently buggy).
    
    Adding libtool to the toolchain also makes sure that it always exists
    when building packages instead of having to depend on the build-tools
    libtool.

--- packages/autotools-reconf.mk
+++ packages/autotools-reconf.mk
@@ -1,8 +1,10 @@
 include packages/autotools.mk
 
+ACLOCAL_FLAGS = -I $(srctree)/toolchains/$(TARGET)/usr/share/aclocal
+
 $(pkgtree)/.autoreconfigure:
 	cd $(pkgbuildtree) && \
-		$(env) autoreconf --force --install
+		$(env) autoreconf --force --install $(ACLOCAL_FLAGS)
 	$(call cmd,stamp)
 
 $(pkgtree)/.configure: $(pkgtree)/.autoreconfigure
