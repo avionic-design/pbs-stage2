616d11c8cea5047f317eec8ccc64afe0f92f64af
Alban Bedel <alban.bedel@avionic-design.de>
2013-12-05 16:02:07 +0100

    packages: Remove the obsolete buildroot
    Change-Id: Ie7a9ed66810b08c23d3ce0b8fa72e14a6befed45
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/1097
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- packages/buildroot/bootstrap/common.mk
+++
@@ -1,8 +0,0 @@
-include packages/buildroot/common.mk
-
-HOST   ?= $(shell support/config.guess)
-BUILD  ?= $(HOST)
-TARGET ?= $(HOST)
-ARCH   ?= $(shell echo $(TARGET) | cut -d- -f1)
-prefix ?= $(bootstrap-prefix)
-
--- packages/buildroot/common.mk
+++
@@ -1,9 +0,0 @@
-# common definitions for packages
-
-set-args = $(foreach arg, $(1), $(arg)='$($(arg))')
-
-bootstrap-prefix ?= /bootstrap
-toolchain-prefix ?= /toolchain
-core-prefix      ?= /core
-devel-prefix     ?= /tools
-
