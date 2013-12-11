61079e20bba570a6cf852a1439c08d6912e85868
Alban Bedel <alban.bedel@avionic-design.de>
2013-12-11 17:56:52 +0100

    build: Make sure the prune step is run when needed
    The prune step should always be run after do-install, however the
    dependcy was missing in the makefiles. Because of this bug the prune
    step was not properly re-run after a partial rebuild.
    
    Change-Id: Ib26d175701def6a08dad492490df2719bfe648c8
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/850
    Reviewed-by: Soeren Grunewald <soeren.grunewald@avionic-design.de>

--- packages/build-tools/cleanup.mk
+++ packages/build-tools/cleanup.mk
@@ -1,6 +1,6 @@
 prune := -f $(if $(KBUILD_SRC),$(srctree)/)packages/build-tools/prune.mk obj
 
-$(pkgtree)/.prune:
+$(pkgtree)/.prune: $(pkgtree)/.do-install
 	$(Q)$(MAKE) $(prune)=$(obj)
 	$(call cmd,stamp)
 
--- packages/prune.mk
+++ packages/prune.mk
@@ -2,7 +2,7 @@ prune-pkg := -f $(if $(KBUILD_SRC),$(srctree)/)scripts/Makefile.prune package
 export prunefiles
 export prunedirs
 
-$(pkgtree)/.prune:
+$(pkgtree)/.prune: $(pkgtree)/.do-install
 	$(Q)$(MAKE) --no-print-directory $(prune-pkg)=$(obj)
 	$(call cmd,stamp)
 
