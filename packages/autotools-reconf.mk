dcd3fbc8388bc3d69632b98eb5eb9bfe2a5efd25
Alban Bedel <alban.bedel@avionic-design.de>
2014-07-09 15:56:14 +0200

    build: Always reconfigure autoconf packages built from sources
    Source link are most commonly used with git checkouts. Such a checkout
    need a reconfigure, even if the normal package doesn't need it.
    
    This patch enable secondary expension in the makefile as $(local-src)
    isn't defined when autotools.mk is parsed. This shouldn't cause any
    problem, but to make sure I first tested this in my local tree for a
    few months before submitting it.
    
    Change-Id: I88b80afeac888104c8cbb31f92a7228758590b08
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/1747
    Reviewed-by: Hendrik Lippek <hendrik.lippek@avionic-design.de>

--- packages/autotools-reconf.mk
+++ packages/autotools-reconf.mk
@@ -1,10 +1,5 @@
 include packages/autotools.mk
 
-$(pkgtree)/.autoreconfigure:
-	cd $(pkgbuildtree) && \
-		$(env) autoreconf --force --install $(ACLOCAL_FLAGS)
-	$(call cmd,stamp)
-
 $(pkgtree)/.configure: $(pkgtree)/.autoreconfigure
 
 conf-args += \
--- packages/autotools.mk
+++ packages/autotools.mk
@@ -1,5 +1,10 @@
 include packages/autotools-base.mk
 
+$(pkgtree)/.autoreconfigure:
+	cd $(pkgbuildtree) && \
+		$(env) autoreconf --force --install $(ACLOCAL_FLAGS)
+	$(call cmd,stamp)
+
 $(pkgtree)/.configure: $(pkgtree)/.patch
 	mkdir -p $(pkgtree)/build/obj-$(HOST_GNU_TYPE) && \
 		cd $(pkgtree)/build/obj-$(HOST_GNU_TYPE) && \
@@ -17,3 +22,6 @@ $(pkgtree)/.do-install: $(pkgtree)/.build
 	cd $(pkgtree)/build/obj-$(HOST_GNU_TYPE) && \
 		$(env) $(priv) $(MAKE) $(install-args) $(install-target)
 	$(call cmd,stamp)
+
+.SECONDEXPANSION:
+$(pkgtree)/.configure: $$(if $$(wildcard $$(local-src)),$(pkgtree)/.autoreconfigure)
