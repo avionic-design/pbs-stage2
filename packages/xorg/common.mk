121f03088d7087175f0fab95bfff0231a25710f5
Alban Bedel <alban.bedel@avionic-design.de>
2014-12-01 11:46:54 +0100

    packages/xorg: Take care of the malloc(0) test
    Various packages in xorg rely on malloc(0) != NULL and some attempts
    to check that in their configure script. As the check doesn't work
    when cross-compiling just assume this is only the case with glibc.
    
    Change-Id: I70b306428df7a8b6a7af24e60c4c0c0580d43e7c
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/2327
    Reviewed-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>

--- packages/xorg/common.mk
+++ packages/xorg/common.mk
@@ -4,3 +4,11 @@ TARBALLS ?= $(PACKAGE)-$(VERSION).tar.bz2
 BUILD_SCRIPT_DIR ?= packages
 BUILD_SCRIPT ?= autotools.mk
 include $(BUILD_SCRIPT_DIR)/$(BUILD_SCRIPT)
+
+ifeq ($(CONFIG_LIBC),gnu)
+conf-vars += \
+	xorg_cv_malloc0_returns_null=no
+else
+conf-vars += \
+	xorg_cv_malloc0_returns_null=yes
+endif
