30f3fd7751269a9404ff0eca8f78aec6b8a5d40d
Bert van Hall <bert.vanhall@avionic-design.de>
2017-01-20 11:23:13 +0100

    packages/ti: move to packages/vendor/ti
    Change-Id: Ic0ff929a9321e729a8cb64931deabfa7d1b8233d
    Signed-off-by: Bert van Hall <bert.vanhall@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/7383
    Reviewed-by: Dirk Leber <dirk.leber@avionic-design.de>

---
+++ packages/vendor/ti/ti.mk
@@ -0,0 +1,29 @@
+# Common definitions for the PBS makefile
+
+include packages/common.mk
+include packages/vendor/ti/make-args.mk
+
+pkgbuildtree   := $(pkgtree)/$(or $(TI_PACKAGE),$(PACKAGE))_$(or $(TI_VERSION),$(VERSION))
+makefile-build := $(srctree)/packages/vendor/ti/$(PACKAGE)/Makefile.build
+
+$(pkgtree)/.setup:
+	$(call cmd,stamp)
+
+$(pkgtree)/.configure: $(makefile-build) $(pkgtree)/.patch
+	cp $< $(pkgbuildtree)
+	$(call cmd,stamp)
+
+$(pkgtree)/.build: $(pkgtree)/.configure
+	mkdir -p $(pkgbuildtree)/build && \
+		cd $(pkgbuildtree)/build && \
+			$(env) $(MAKE) -j $(NUM_CPU) -f ../Makefile.build $(make-args)
+	$(call cmd,stamp)
+
+$(pkgtree)/.do-install: $(pkgtree)/.build
+	mkdir -p $(pkgbuildtree)/build && \
+		cd $(pkgbuildtree)/build && \
+			$(env) $(MAKE) -j $(NUM_CPU) -f ../Makefile.build $(make-args) install
+	$(call cmd,stamp)
+
+include packages/cleanup.mk
+include packages/binary.mk
